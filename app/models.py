0f7cac5e64b40d63c242f8fb5091f3f616ed99ba
appanywork-eng <app.anywork@gmail.com>
2025-11-11 14:23:16 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from .database import Base
 
-
 class User(Base):
     __tablename__ = "users"
 
@@ -10,7 +9,6 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-
 
 class Confession(Base):
     __tablename__ = "confessions"
