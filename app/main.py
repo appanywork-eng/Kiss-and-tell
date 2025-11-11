e7cda767ca7117006022e70e8523c0475bfa962e
appanywork-eng <app.anywork@gmail.com>
2025-11-11 11:55:13 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from app.auth import router as auth_router
+from app.confession import router as confession_router
+from app.database import Base, engine
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(confession_router)
+
+@app.get("/")
+def home():
+    return {"message": "Backend Running ✅"}
