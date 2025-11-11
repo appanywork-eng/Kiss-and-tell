1bb637c34ae44e372b44d637a9c134634c995fe5
appanywork-eng <app.anywork@gmail.com>
2025-11-11 14:22:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,13 @@ from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.confession import router as confession_router
 from app.database import Base, engine
-import app.models   # ✅ needed so table definitions load
+import app.models  # <-- important
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 app.include_router(auth_router)
 app.include_router(confession_router)
-
 
 @app.get("/")
 def home():
